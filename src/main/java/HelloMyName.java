1b9828700c969db8868c2620835d5167c688778a
Khummeung Wattanasaroj <62160244@go.buu.ac.th>
2021-07-31 19:15:13 +0700

    create Player Table and recreate Main class and build HelloMyName class

--- src/main/java/HelloMyName.java
+++ src/main/java/HelloMyName.java
@@ -26,22 +26,81 @@ public class HelloMyName extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        getNameInputField = new javax.swing.JTextField();
+        okButton = new javax.swing.JButton();
+        display1 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Hello My Name");
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel1.setText("Enter Name :");
+
+        getNameInputField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                getNameInputFieldActionPerformed(evt);
+            }
+        });
+
+        okButton.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        okButton.setText("OK");
+        okButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                okButtonActionPerformed(evt);
+            }
+        });
+
+        display1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        display1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        display1.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        display1.setVerifyInputWhenFocusTarget(false);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(display1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(160, 160, 160)
+                        .addComponent(okButton)
+                        .addGap(0, 162, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addGap(20, 20, 20)
+                        .addComponent(jLabel1)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(getNameInputField)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(getNameInputField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(okButton)
+                .addGap(51, 51, 51)
+                .addComponent(display1, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(116, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void getNameInputFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_getNameInputFieldActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_getNameInputFieldActionPerformed
+
+    private void okButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_okButtonActionPerformed
+        String name = getNameInputField.getText();
+        display1.setText("Hello " +name+"!!!");
+    }//GEN-LAST:event_okButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +138,9 @@ public class HelloMyName extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel display1;
+    private javax.swing.JTextField getNameInputField;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton okButton;
     // End of variables declaration//GEN-END:variables
 }
